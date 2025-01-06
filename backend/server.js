3350b2077f727c2fc64e2a76c72486eff79d9620
Meir <friedmannmeir@gmail.com>
2025-01-06 19:58:20 +0200

    testing express

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/backend', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
 });
 
 app.listen(port, () => {
