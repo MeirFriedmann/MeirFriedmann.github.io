9cd7140303cbd8f42d7de90768eec0d6a666b86a
Meir <friedmannmeir@gmail.com>
2025-01-06 21:04:31 +0200

    removed  static path

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,17 @@ const port = 5000;
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "assets")));
-
 app.get('/backend', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
 
 app.get('/image', (req, res) => {
+  const imagePath = path.join(__dirname, 'assets', 'profile.webp');
   res.set({
     "Content-Type": "image/webp",
     "Cache-Control": "public, max-age=3600"
   });
-  res.sendFile('./profile.webp');
+  res.sendFile(imagePath);
 });
 
 app.listen(port, () => {
