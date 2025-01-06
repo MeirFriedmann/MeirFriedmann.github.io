2b8f7020ff57aa86e0078025cf3944848a129401
Meir <friedmannmeir@gmail.com>
2025-01-07 01:57:29 +0200

    fixing browsers sus

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,19 @@
 document.getElementById("myButton").onclick = function() {
     alert("Hello, World!");
 };
+
+async function loadImage(ipUrl) {
+    try {
+      const response = await fetch(ipUrl, {
+        mode: 'cors',
+      });
+      const blob = await response.blob();
+      const imgUrl = URL.createObjectURL(blob);
+      document.querySelector('img').src = imgUrl;
+    } catch (error) {
+      console.error('Image loading failed:', error);
+    }
+  }
+
+  const ipUrl = 'https://143.244.184.83/image';
+  loadImage(ipUrl);
